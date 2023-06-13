7a13248f9df46b84af8092065c7deb1b49a62dcb
MuntasirMac <Muntasir120@gmail.com>
2023-06-13 22:55:41 +0600

    initialised and installed dependencies and wrote some code

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, url_for
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/chat')
+def chat():
+    username = request.args.get('username')
+    room = request.args.get('room')
+
+    if username and room:
+        return render_template("chat.html", username=username, room=room)
+    else:
+        return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
