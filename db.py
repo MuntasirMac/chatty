66b76b9008fea0fb68cf7b52bb8cb3fe6ddafac4
MuntasirMac <Muntasir120@gmail.com>
2023-06-15 00:41:44 +0600

    configured database cluster and added gitignore

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
+from decouple import config
 from werkzeug.security import generate_password_hash
 
-client = MongoClient("url will be provided after getting the cluster")
+client = MongoClient(config('mongo_uri'))
 
-chat_db = client.get_database("will be generated")
-users_collection = chat_db.get_collection("will be provided after setting the cluster")
+chat_db = client.get_database(config('db'))
+users_collection = chat_db.get_collection("users")
 
 def save_user(username, email, password):
     password_hash = generate_password_hash(password)
@@ -14,3 +15,5 @@ def save_user(username, email, password):
         'password': password_hash
     }
     users_collection.insert_one(payload_to_insert)
+
+save_user("mac", "mac@test.com", "test_pass")
